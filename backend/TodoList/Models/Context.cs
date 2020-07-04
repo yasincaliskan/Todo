7adac5966cd20f68fc737fd9521769acbd99322f
Yasin Caliskan <yasincaliskan26@yahoo.com>
2020-07-04 18:35:19 +0300

    First commit

---
+++ backend/TodoList/Models/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Entities;
+
+namespace TodoList.Models
+{
+    public class Context : DbContext
+    {
+       
+        public DbSet<User> Users { get; set; }
+        public DbSet<Todo> Todos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-Q22PP2J;database=TodoListDB;trusted_connection=true;");
+        }
+    }
+}
---
+++ backend/TodoList/Models/Entities/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Entities
+{
+    public class Todo
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int UserID { get; set; }
+        public User User { get; set; }
+    }
+}
