74e93a9008c77ab9f3d6ab4c417398170d54e56f
Yasin Caliskan <yasincaliskan26@yahoo.com>
2020-07-14 21:09:58 +0300

    Updated Home Page

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,48 +1,19 @@
 import React from 'react';
 import Login from "./Login";
 import Register from "./Register";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import './style.css';
+
 
 function Home() {
+
     return (
         <div>
             <h2>Welcome to ToDo!</h2>
-
-            <Router>
-                <div>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/login">Login</Link>
-                        </li>
-                        <li>
-                            <Link to="/register">Register</Link>
-                        </li>
-                    </ul>
-
-                    <Switch>
-                        <Route path="/login">
-                            <Login />
-                        </Route>
-                        <Route path="/register">
-                            <Register />
-                        </Route>
-                        <Route path="/">
-                            <Home />
-                        </Route>
-                    </Switch>
-                </div>
-            </Router>
-
+            <button style={{ background: "green" }} type="button">Sign In</button>
+            <button style={{ background: "orange" }}>Sign Up</button>
         </div>
     )
+
 
 }
 
